8d16dbb94524f8680445d5a90f8d8da73c7a81d4
johnlaz <51280623+johnlaz@users.noreply.github.com>
2026-04-16 03:06:59 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'davinci-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'davinci-v3.4.0';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
